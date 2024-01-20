309a660e92082e24ba09ee22aab8c8b5985be0c7
asadbek000002 <asadbektuygunov9@gmail.com>
2024-01-20 11:36:05 +0500

    Fati

---
+++ aboutInstitut/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AboutinstitutConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aboutInstitut'
---
+++ meteng/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MetengConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meteng'
