408b183da161ac1b3ec47d5d4476fc39efebc67a
asadbek000002 <asadbektuygunov9@gmail.com>
2024-01-20 14:41:06 +0500

    abount Institut models.py tayyor

--- aboutInstitut/admin.py
+++ aboutInstitut/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Leadership, LeadershipImage
 
-# Register your models here.
+
+class LeadershipInline(admin.TabularInline):
+    model = LeadershipImage
+    extra = 0
+    fields = ('image',)
+    max_num = 10
+    min_num = 1
+    can_delete = False
+    show_change_link = True
+    show_add_button = True
+    verbose_name = 'Image'
+    verbose_name_plural = 'Images'
+    list_display = ('image',)
+    ordering = ('image',)
+
+
+class LeadershipAdmin(admin.ModelAdmin):
+    inlines = [LeadershipInline]
+    list_display = ['position', 'full_name', 'degree', 'phone_number', 'email', 'reception_days', 'image']
+
+
+admin.site.register(Leadership, LeadershipAdmin)
