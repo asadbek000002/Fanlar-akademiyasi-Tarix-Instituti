d31b8d3f604302eb07d1173e166d943e557d5d7d
asadbek000002 <asadbektuygunov9@gmail.com>
2024-01-22 15:28:53 +0500

    Jazzimn and internetionalContact models.py

--- internetionalContact/admin.py
+++ internetionalContact/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import InternationalResearchers, InternationalResearchersExpert, \
+                    InternationalProjects
 
-# Register your models here.
+
+class InternationalResearchersInline(admin.TabularInline):
+    model = InternationalResearchersExpert
+    fk_name = 'researchers'
+
+
+@admin.register(InternationalResearchers)
+class InternationalResearchersAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image']
+    list_filter = ['title', 'image']
+    inlines = [InternationalResearchersInline]
+
+
+@admin.register(InternationalProjects)
+class InternationalProjectsAdmin(admin.ModelAdmin):
+    list_display = ['project_name', 'partner_organization', 'funding_organization', 'done_undone_year',
+                    'project_status', 'image']
+
+
--- internetionalContact/models.py
+++ internetionalContact/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_time = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+
+class PartnerOrganizations(BaseModel):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/internetionalContact/partner_organizations')
+    context = models.TextField()
+
+
+# XALQARO LOYHALAR
+class InternationalProjects(BaseModel):
+    PROJECT_STATUS = (
+        ('bajarilgan', 'Bajarilgan'),
+        ('bajarilmagan', 'Bajarilmagan'),
+    )
+
+    project_name = models.CharField(max_length=250)
+    partner_organization = models.CharField(max_length=250)
+    funding_organization = models.CharField(max_length=250)
+    done_undone_year = models.IntegerField()
+    project_status = models.CharField(max_length=20, choices=PROJECT_STATUS, default='bajarilmagan')
+    image = models.ImageField(upload_to='images/internetionalContact/international_projects')
+
+    class Meta:
+        verbose_name = 'International Project'
+        verbose_name_plural = 'International Projects'
+
+    def __str__(self):
+        return self.project_name
+
+
+class InternationalResearchers(BaseModel):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/internetionalContact/international_researchers')
+
+
+class InternationalResearchersExpert(BaseModel):
+    researchers = models.ForeignKey(InternationalResearchers, on_delete=models.CASCADE,
+                                                        related_name='researchers')
+    full_name = models.CharField(max_length=250)
+    activity_institution = models.CharField(max_length=350)
+    year_of_visit = models.IntegerField()
